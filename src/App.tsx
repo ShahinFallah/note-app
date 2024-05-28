186d332244eef2e16f51a7785837dfbbbe4d9349
ShahinFallah <shahinfallah2006@gmail.com>
2024-05-28 10:40:12 +0330

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { Navigate, Route, Routes } from "react-router-dom"
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl">App</h1>
-    </>
+    <div className="my-4">
+      <Routes>
+        <Route path="/" element={<h1 className="text-2xl">Home</h1>} />
+        <Route path="/new" element={<h1 className="text-2xl">new</h1>} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to='/' />} />
+      </Routes>
+    </div>
   )
 }
 
